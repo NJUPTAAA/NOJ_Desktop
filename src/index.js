4f5bbf321d0483bc2399043adbe0639c8910b9e6
ZsgsDesign <zsgsdesign@gmail.com>
2020-05-01 14:58:30 +0800

    add core js code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow,screen } = require('electron');
 const path = require('path');
 
 if (require('electron-squirrel-startup')) {
@@ -6,41 +6,34 @@ if (require('electron-squirrel-startup')) {
 }
 
 const createWindow = () => {
+    const {width, height} = screen.getPrimaryDisplay().workAreaSize
     const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        minWidth: width-100,
+        minHeight: height-100,
+        center: true,
         webPreferences: {
             nodeIntegration: true
-        }
+        },
+        show: false
     });
+    mainWindow.maximize();
 
     mainWindow.loadFile(path.join(__dirname, 'index.html'));
-
-    // Open the DevTools.
-    // mainWindow.webContents.openDevTools();
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show();
+    });
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
         app.quit();
     }
 });
 
 app.on('activate', () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) {
         createWindow();
     }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
